0999ab0f88144ce6d57cd8479c2dbff982f53e20
Daryk at school <daryk.kohler@gmail.com>
2016-11-16 16:54:22 -0800

    Small change to an ApacheHelper script.

--- CopyFromRepoToAllTestAndApache.sh
+++ CopyFromRepoToAllTestAndApache.sh
@@ -2,5 +2,8 @@
 
 cp -ruvp AllTest/ ~/Documents/.
 cp -ruvp ApacheHelpers/images /var/www/html/.
+cp -ruvp ApacheHelpers/images ~/Source/firebase/public/.
 cp -ruvp ApacheHelpers/css /var/www/html/.
+cp -ruvp ApacheHelpers/css ~/Source/firebase/public/.
 cp -ruvp ApacheHelpers/js /var/www/html/.
+cp -ruvp ApacheHelpers/js ~/Source/firebase/public/.
